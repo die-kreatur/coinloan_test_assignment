a2d163418d5893c376fc8d0374f85fee48a3bcb3
die-kreatur <parusmajor@list.ru>
2022-07-10 23:58:08 +0300

    Add module to fetch quotes via websocket

--- coinloan/src/main.rs
+++ coinloan/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 mod binance_client;
+mod get_prices;
 
 
 #[tokio::main]
@@ -8,11 +9,15 @@ async fn main() {
 	dotenv::dotenv().ok();
 
 	use binance_client::binance_client::Binance;
+	use get_prices::get_prices::fetch_tickers;
 
 	let acc = Binance {
 		api_key: env::var("KEY").unwrap(),
 		api_secret: env::var("SECRET").unwrap()
 	};
+
+	let resp = fetch_tickers();
+	println!("{:#?}", resp);
 
 	let resp = acc.send_limit_order("blabla", "bla", "GTC", "bla", "bla").await;
     println!("Data: {}", resp);

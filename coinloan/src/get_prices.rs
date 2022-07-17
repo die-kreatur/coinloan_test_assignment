2f54ae6fb2c97a401e9a60a6431c7e34d2921be1
die-kreatur <parusmajor@list.ru>
2022-07-17 13:24:18 +0300

    Add deserializer

--- coinloan/src/get_prices.rs
+++ coinloan/src/get_prices.rs
@@ -1,26 +1,34 @@
 use serde_json;
-use serde::Deserialize;
+use serde::de;
+use serde::{Deserialize, Deserializer};
 use tungstenite::connect;
 use tungstenite::Message::Text as WsResponse;
 
 
-const WS_STREAM: &str = "wss://stream.binance.com:9443/stream?streams=";
-
-#[derive(Deserialize, Debug)]
-pub struct ParsedData {
-    pub data: Ticker
+#[derive(Deserialize)]
+struct ParsedData {
+    data: Ticker
 }
 
-#[derive(Deserialize, Debug)]
-pub struct Ticker {
+#[derive(Deserialize)]
+struct Ticker {
     #[serde(rename = "s")]
-    pub symbol: String,
+    symbol: String,
     #[serde(rename = "c")]
-    pub price: String
+    #[serde(deserialize_with = "string_to_f64")]
+    price: f64
 }
 
-pub fn fetch_tickers(ticker: &str) -> ParsedData {
-    let mut ws_stream = format!("wss://stream.binance.com:9443/stream?streams={}miniTicker", ticker);
+fn string_to_f64<'a, D>(deserializer: D) -> Result<f64, D::Error>
+where
+    D: Deserializer<'a>,
+{
+    let str_num = String::deserialize(deserializer).unwrap();
+    str_num.parse::<f64>().map_err(de::Error::custom)
+}
+
+pub fn fetch_tickers(ticker: &str, to_compare: f64) -> f64 {
+    let ws_stream = format!("wss://stream.binance.com:9443/stream?streams={}miniTicker", ticker);
 
     let (mut ws, _) = 
         connect(ws_stream).expect("Cannot establish connection");
@@ -36,7 +44,9 @@ pub fn fetch_tickers(ticker: &str) -> ParsedData {
         
         let parsed_data: ParsedData = 
             serde_json::from_str(&msg).expect("Unable to parse message");
-        
-        return parsed_data
+
+        if parsed_data.data.price >= to_compare {
+            return parsed_data.data.price
+        }
     }
 }

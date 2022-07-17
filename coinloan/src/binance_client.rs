23ddf8d9e80fd8644a230cf5de77914d7f1db182
die-kreatur <parusmajor@list.ru>
2022-07-17 16:58:30 +0300

    Add method to monitor limit order

--- coinloan/src/binance_client.rs
+++ coinloan/src/binance_client.rs
@@ -3,6 +3,8 @@ use reqwest::{header, blocking::Client as Client};
 use sha2::Sha256;
 use std::time::{SystemTime, UNIX_EPOCH};
 use serde_json::Value;
+use tungstenite::connect;
+use tungstenite::Message::Text as WsResponse;
 
 
 pub struct Binance {
@@ -64,7 +66,7 @@ impl Binance {
         data
     }
 
-    pub fn get_listen_key(&self) -> String {
+    fn get_listen_key(&self) -> String {
         let client = self.get_client();
         let response = client
             .post("https://api.binance.com/api/v1/userDataStream")
@@ -74,5 +76,32 @@ impl Binance {
         let listen_key: Value = response.json().unwrap();
 
         listen_key["listenKey"].to_string()
+    }
+
+    pub fn is_limit_order_completed(&self) -> bool {
+        let listen_key = self.get_listen_key();
+        let user_stream = format!("wss://stream.binance.com:9443/ws/{}", listen_key);
+
+        let (mut ws, _) = 
+        connect(user_stream).expect("Cannot establish connection");
+
+        loop {
+            let msg = ws.read_message().expect("Cannot read message");
+            let msg = match msg {
+                WsResponse(m) => m,
+                _ => {
+                        panic!("Response is invalid");
+                    }
+                };
+            
+            let parsed_data: Value = 
+                serde_json::from_str(&msg).expect("Unable to parse message");
+
+            if parsed_data["e"].as_str().unwrap() == "executionReport" {
+                if parsed_data["X"].as_str().unwrap() == "FILLED" {
+                    return true
+                }
+            }
+        }
     }
 }
